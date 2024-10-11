5ae2912f7ec5a714df1cbca2824284f4b4397249
Aarav Goyal <gl.aarav@gmail.com>
2024-10-11 14:06:13 -0700

    Update RollDiceV2.java

--- RollDiceV2.java
+++ RollDiceV2.java
@@ -1,11 +1,11 @@
 /* Aarav Goyal
  * 9/27/2024
- * RollDice.java
+ * RollDiceV2.java
  * Working on: using Math.random to make a random # from 1 to 6.
  * Rolling the dice will be a method call
  */
  
- public class RollDice{
+ public class RollDiceV2{
 	 public static void main (String[] args){
 	 int RollDi = 0;
 	 rollDi = roll();
